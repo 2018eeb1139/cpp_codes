945ff715a08bbf95fceda3c4d08332e079a1a873
2018eeb1139 <69035844+2018eeb1139@users.noreply.github.com>
2021-06-04 09:40:04 +0530

    Add files via upload

---
+++ LeetCode/binary_tree_level_order_II.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+         vector<vector<int>> ans;
+        if(root==NULL) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int count=q.size();
+            vector<int> level_nodes;
+            for(int i=0;i<count;i++){
+                TreeNode *temp=q.front();
+                q.pop();
+                level_nodes.push_back(temp->val);
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+            }
+            ans.push_back(level_nodes);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};

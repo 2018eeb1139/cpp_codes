d2c6448564b1700557ee6ee24e99d1eb3dd0ac4d
2018eeb1139 <69035844+2018eeb1139@users.noreply.github.com>
2021-07-11 10:27:38 +0530

    Add files via upload

---
+++ LeetCode/twoSum-input-array-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0;vector<int> res;
+        int j=numbers.size()-1;
+        while(i<=j){
+            int sum=numbers[i]+numbers[j];
+            if(sum>target) j--;
+            else if(sum<target) i++;
+            else {
+                res.push_back(i+1);res.push_back(j+1);break;
+            }
+        }
+        return res;
+    }
+};

b19b0fcd2f108fb9774e0fbf6f46626f9826a1e6
2018eeb1139 <69035844+2018eeb1139@users.noreply.github.com>
2021-07-11 10:28:19 +0530

    Add files via upload

---
+++ HackerRank/binaryExponentiation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int M=1e9+7;
+
+int binaryExponentiation(int a,int b){
+    if(b==0) return 1;
+    long res=binaryExponentiation(a,b/2);
+    if(b&1) return ((a*(res*res)%M)%M);
+    else return ((res*res)*M);
+}
+
+int main()
+{
+    int a=2,b=13;
+    cout<<binaryExponentiation(a,b);
+}

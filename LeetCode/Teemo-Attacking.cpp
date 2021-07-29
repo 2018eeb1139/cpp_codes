8361b1dc3f7be1486eb0588faaa8ec1dd31caa71
Aman Chandra <69035844+2018eeb1139@users.noreply.github.com>
2021-07-29 19:53:12 +0530

    Add files via upload

---
+++ LeetCode/Teemo-Attacking.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int totalTime=0;
+        if(duration==0) return 0;
+        for(int i=0;i<timeSeries.size()-1;i++){
+            totalTime+=min(timeSeries[i+1]-timeSeries[i],duration);
+        }
+        return totalTime+duration;
+    }
+};

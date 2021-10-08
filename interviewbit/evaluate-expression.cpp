8bd204e05966f101fa2eafa6f19727318a3c481d
Aman Chandra <69035844+2018eeb1139@users.noreply.github.com>
2021-10-09 00:05:16 +0530

    Add files via upload

---
+++ interviewbit/evaluate-expression.cpp
@@ -0,0 +1,15 @@
+int Solution::evalRPN(vector<string> &A) {
+    stack<int> nums;
+    for(string& op:A){
+        if(op=="+" or op=="-" or op=="*" or op=="/"){
+            int b=nums.top();nums.pop();
+            int a=nums.top();nums.pop();
+            if(op=="-") nums.push(a-b);
+            else if(op=="+") nums.push(a+b);
+            else if(op=="*") nums.push(a*b);
+            else if(op=="/") nums.push(a/b);
+        }
+        else nums.push(stoi(op));
+    }
+    return nums.top();
+}

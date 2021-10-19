1bd7bc206956d6b8faa2acfd11f018b3c0232501
Aman Chandra <69035844+2018eeb1139@users.noreply.github.com>
2021-10-19 11:43:09 +0530

    Add files via upload

---
+++ interviewbit/remove-half-nodes.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::solve(TreeNode* root) {
+    if(root==NULL) return NULL;
+    if(root->left==NULL and root->right==NULL) return root;
+    if(root->left==NULL) return solve(root->right);
+    if(root->right==NULL) return solve(root->left);
+    root->left=solve(root->left);
+    root->right=solve(root->right);
+    return root;
+}
